c1bbc078ad7596d6bd6b34a8a5257632d185cc52
Idris Elgarrab <125475208+AnarchyNinetyNine@users.noreply.github.com>
2023-04-25 23:37:05 +0200

    Update sourceCode.py

--- sourceCode.py
+++ sourceCode.py
@@ -1,4 +1,6 @@
-We have designed a Convolutional Neural Network (CNN) using TensorFlow's Keras API to address a specific classification problem. The architecture of the model is carefully chosen to effectively learn hierarchical features from the input images and perform accurate classification tasks. The model consists of three pairs of convolutional and max-pooling layers, a flatten layer, and two dense layers, with each layer serving a specific purpose and having selected parameters to optimize performance.
+UltarSonic.
+
+We have designed a `Convolutional Neural Network (CNN)` using TensorFlow's Keras API to address a specific classification problem. The architecture of the model is carefully chosen to effectively learn hierarchical features from the input images and perform accurate classification tasks. The model consists of three pairs of convolutional and max-pooling layers, a flatten layer, and two dense layers, with each layer serving a specific purpose and having selected parameters to optimize performance.
 
 The first convolutional layer has 32 filters, a 3x3 kernel size, ReLU activation function, and an input shape of (224, 224, 3) representing 224x224 pixel images with 3 color channels (RGB). We chose 32 filters as a starting point to balance computational efficiency and the ability to learn various low-level features. The 3x3 kernel size is a common choice because it can effectively capture local patterns while reducing computational complexity compared to larger kernel sizes.
 
